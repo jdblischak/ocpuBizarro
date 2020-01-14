73a53a50aeb012ca233490cd73d2070c52703d36
John Blischak <john.blischak@abbvie.com>
2020-01-14 13:07:13 -0500

    Use consistent naming for data types.

--- inst/www/js/bizarro-boolean.js
+++ inst/www/js/bizarro-boolean.js
@@ -1,18 +1,18 @@
 //init this script when the page has loaded
 $(document).ready(function(){
-  $("#submitbuttonlogical").on("click", function(){
-    $("#submitbuttonlogical").attr("disabled", "disabled");
-    var mylogical = ($("#logicalfield").val() == "true");
+  $("#submitbuttonboolean").on("click", function(){
+    $("#submitbuttonboolean").attr("disabled", "disabled");
+    var myboolean = ($("#booleanfield").val() == "true");
     var req = ocpu.rpc("bizarro", {
-      x : mylogical
+      x : myboolean
     }, function(output){
-      $("#outputlogical").text(output);
+      $("#outputboolean").text(output);
     });
     req.fail(function(){
       alert("Server error: " + req.responseText);
     });
     req.always(function(){
-      $("#submitbuttonlogical").removeAttr("disabled")
+      $("#submitbuttonboolean").removeAttr("disabled")
     });
   });
 });
